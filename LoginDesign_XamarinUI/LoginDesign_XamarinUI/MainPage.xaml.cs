776c1d9dfc1e85c89a29591aac4b16cd72645098
Lucas Gabriel <pintolucas996@gmail.com>
2023-10-05 13:35:19 -0300

    Primeiro commit

---
+++ LoginDesign_XamarinUI/LoginDesign_XamarinUI/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginDesign_XamarinUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            if(txtUsername.Text == "admin" && txtPassword.Text == "123")
+            {
+
+                Navigation.PushAsync(new HomePage());
+
+            }
+            else
+            {
+                DisplayAlert("Ops..", "Nome do usuário ou senha incorretos!", "Ok");
+            }
+
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+
+            Navigation.PushAsync(new RegisterPage());
+
+        }
+    }
+}
